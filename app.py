e560bb40ddffedad59fb2846bf7ac272cc6f4730
Bbmxzz <pichayapa1147@gmail.com>
2024-06-17 14:23:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def submit():
             return jsonify(status= 'failed')
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=5000, debug=True)
+   app.run(host="0.0.0.0", port=10000, debug=True)
