d577e554d410d8bc06458a3b05d83aa646efabc9
Bbmxzz <pichayapa1147@gmail.com>
2024-06-18 17:20:09 +0700

    logo

--- static/js/gotocontact.js
+++ static/js/gotocontact.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-    // Smooth scrolling
     $('a[href^="#"]').on('click', function(event) {
         var target = $(this.getAttribute('href'));
         if(target.length) {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
-            }, 1000); // Adjust the duration here (1000 ms = 1 second)
+            }, 1000);
         }
     });
 });
